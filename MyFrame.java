ffa6ea4d9deb8f3a36a3f63be45f5fff4f482068
Brian Nguyen <bnguyen9991@outlook.com>
2022-11-28 07:42:34 -0700

    big chilling

---
+++ MyFrame.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MyFrame{
+    public static void main (String []args){
+        JFrame frame = new JFrame(); // creates a frame
+        frame.setTitle("Gui Time"); // set title
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of app
+        frame.setResizable(true); // able change size
+        frame.setSize(420,420); // set the x-dimension, and y-dimension of frame
+        frame.setVisible(true); // makes frame visible
+        frame.getContentPane().setBackground(Color.blue); // change color of background
+        JLabel bofa = new JLabel ("bofa");
+        frame.add(bofa);
+
+        JButton b = new JButton ("bofa");
+        b.setPrefferedSize(new Dimension (100,30));
+        frame.add (b);
+        frame.setSize(500, 500);
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+    }
+}
